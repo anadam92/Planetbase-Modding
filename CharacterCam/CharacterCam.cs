1e3198d38eda847f1618fbf6031cc8c467c941af
João Farias <jpfarias9@gmail.com>
2016-03-21 21:47:09 +0000

    Added CharacterCam and Redirector; Updated reference library to beta16

---
+++ CharacterCam/CharacterCam.cs
@@ -0,0 +1,30 @@
+﻿using Planetbase;
+using Redirection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace CharacterCam
+{
+    public class CharacterCam : IMod
+    {
+        public void Init()
+        {
+            Redirector.PerformRedirections();
+            Debug.Log("[MOD] CharacterCam activated");
+        }
+
+        public void Update() {}
+    }
+
+    public abstract class CustomCharacter : Character
+    {
+        [RedirectFrom(typeof(Character))]
+        public override bool isCloseCameraAvailable()
+        {
+            return true;
+        }
+    }
+}
