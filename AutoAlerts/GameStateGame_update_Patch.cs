f93cf9e05756add5018597894de346dff76ca2e0
Andronis Adamantios <adamantios.andronis@outlook.com.gr>
2021-11-07 16:23:02 +0000

    done AutoAlerts and AutoConnections using harmony Patches
    Main.cs same for both and as in DawnOfMan
    Main.cs same for everything so far

---
+++ AutoAlerts/GameStateGame_update_Patch.cs
@@ -0,0 +1,77 @@
+﻿using Planetbase;
+using System.Collections.Generic;
+using HarmonyLib;
+
+namespace AutoAlerts {
+
+    [HarmonyPatch(typeof(GameStateGame), "update", MethodType.Normal)]
+    class GameStateGame_update_Patch {
+
+        /// <summary>
+        /// mod specific: true if the planetbase alert has been auto-activated (activated by this mod), false if activated manually by the player
+        /// </summary>
+        private static bool m_autoActivated;
+
+        /// <summary>
+        /// mod specific: the type of the alert
+        /// </summary>
+        private static AlertState m_activatedState;
+
+        [HarmonyPostfix]
+        public static void Postfix() {
+            if (ConstructionComponent.findOperational(TypeList<ComponentType, ComponentTypeList>.find<SecurityConsole>()) == null)
+                return;
+
+            AlertState state = SecurityManager.getInstance().getAlertState();
+
+            // if the state has been changed manually, don't do anything else. Will be activated again if the player sets NoAlert
+            if (state != m_activatedState) {
+                m_activatedState = AlertState.NoAlert;
+                m_autoActivated = false;
+                return;
+            }
+
+            List<Character> intruders = Character.getSpecializationCharacters(SpecializationList.IntruderInstance);
+            if (intruders != null) {
+                foreach (Character intruder in intruders) {
+                    if (intruder.hasStatusFlag(Character.StatusFlagDetected)) {
+                        // check number of guards vs intruders - want to keep on yellow while ratio guards/intruders is high enough
+                        float numIntruders = intruders.Count;
+                        float numGuards = Character.getCountOfSpecialization(TypeList<Specialization, SpecializationList>.find<Guard>());
+
+                        float ratio = numGuards / numIntruders;
+                        AlertState newState = ratio < 0.75f ? AlertState.RedAlert : AlertState.YellowAlert;
+
+                        if (newState != m_activatedState) {
+                            SecurityManager.getInstance().setAlertState(newState);
+                            m_activatedState = newState;
+                            m_autoActivated = true;
+                        }
+
+                        return;
+                    }
+                }
+            }
+
+            if (DisasterManager.getInstance().anyInProgress()) {
+                if (state != AlertState.YellowAlert) {
+                    SecurityManager.getInstance().setAlertState(AlertState.YellowAlert);
+                    m_activatedState = AlertState.YellowAlert;
+                    m_autoActivated = true;
+                }
+
+                return;
+            }
+
+            if (m_autoActivated) {
+                // Only disable alert if it's the same one we set
+                if (state == m_activatedState)
+                    SecurityManager.getInstance().setAlertState(AlertState.NoAlert);
+
+                m_activatedState = AlertState.NoAlert;
+                m_autoActivated = false;
+            }
+
+        }
+    }
+}
---
+++ AutoConnections/GameStateGame_onModulePlacementEnd_Patch.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using Planetbase;
+using System.Collections.Generic;
+
+namespace AutoConnections {
+
+    [HarmonyPatch(typeof(GameStateGame), "onModulePlacementEnd", MethodType.Normal)]
+    public abstract class GameStateGame_onModulePlacementEnd_Patch {
+
+        private static Traverse<Module> t_mActiveModule = Traverse.Create<Module>().Field<Module>("mActiveModule");
+        private static Traverse<List<Module>> t_mModules = Traverse.Create<Module>().Field<List<Module>>("mModules");
+        private static FastInvokeHandler mi_Module_recycleLinkComponents = MethodInvoker.GetHandler(typeof(Module).GetMethod("recycleLinkComponents"));
+        
+        [HarmonyLib.HarmonyPrefix]
+        public static void Prefix() {
+            Module mActiveModule = t_mActiveModule.Value;
+            List<Module> mModules = t_mModules.Value;
+
+            // Add available connections
+            List<Module> linkableModules = new List<Module>();
+            for (int i = 0; i < mModules.Count; i++) {
+                Module module = mModules[i];
+                if (module != null && module != mActiveModule && Connection.canLink(mActiveModule, module, mActiveModule.getPosition(), module.getPosition())) {
+                    linkableModules.Add(module);
+                }
+            }
+
+            if (!mActiveModule.hasFlag(ModuleType.FlagDeadEnd) || linkableModules.Count == 1) {
+                foreach (Module module in linkableModules) {
+                    Connection connection = Connection.create(mActiveModule, module);
+                    connection.onUserPlaced();
+                    connection.setRenderTop(Traverse.Create<GameStateGame>().Field<bool>("mRenderTops").Value);
+                    mi_Module_recycleLinkComponents.Invoke(mActiveModule, new object[] { });
+                    mi_Module_recycleLinkComponents.Invoke(module, new object[] { });
+                }
+            }
+        }
+
+    }
+
+}
