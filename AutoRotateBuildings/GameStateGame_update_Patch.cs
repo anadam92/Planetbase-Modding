03202d440337d35bf19c689f564c22c6eda4c72d
Andronis Adamantios <adamantios.andronis@outlook.com.gr>
2021-11-07 19:44:57 +0000

    AutoRotateBuildings. builds.

---
+++ AutoRotateBuildings/GameStateGame_update_Patch.cs
@@ -0,0 +1,64 @@
+﻿using Planetbase;
+using System.Collections.Generic;
+using HarmonyLib;
+using UnityEngine;
+using System;
+
+namespace AutoAlerts {
+
+    [HarmonyPatch(typeof(GameStateGame), "update", MethodType.Normal)]
+    class GameStateGame_update_Patch {
+
+        /// <summary>
+        /// mod specific:
+        /// </summary>
+        private static int connectionCount = 0;
+
+        [HarmonyPostfix]
+        public static void Postfix() {
+            GameManager gameManager = GameManager.getInstance();
+            GameManager.State mState = Traverse.Create(gameManager).Field<GameManager.State>("mState").Value;
+
+            if (mState != GameManager.State.Updating)
+                return;
+
+            GameStateGame gameStateGame = GameManager.getInstance().getGameState() as GameStateGame;
+            /* private enum GameStateGame.Mode */
+            object mMode
+                = Traverse.Create(gameStateGame).Field("mMode").GetValue();
+            object GameStateGame_Mode_PlacingModule
+                = Traverse.Create<GameStateGame>().Type("Mode").Field("PlacingModule").GetValue();
+            Traverse t_gameStateGame_mActiveModule = Traverse.Create<GameStateGame>().Field("mActiveModule");
+            Module activeModule = t_gameStateGame_mActiveModule.GetValue<Module>();
+
+            if (
+                object.Equals(gameStateGame, null) ||
+                object.Equals(mMode, GameStateGame_Mode_PlacingModule) ||
+                object.Equals(t_gameStateGame_mActiveModule.GetValue(), null)
+                ) {
+                return;
+            }
+
+            
+            List<Vector3> connectionPositions = new List<Vector3>();
+            List<Construction> Construction_mConstructions = Traverse.Create<Construction>().Field<List<Construction>>("mConstructions").Value;
+            for (int i = 0; i < Construction_mConstructions.Count; ++i) {
+                Module module = Construction_mConstructions[i] as Module;
+                if (module != null && module != activeModule && Connection.canLink(activeModule, module)) {
+                    connectionPositions.Add(module.getPosition());
+                }
+            }
+
+            if (connectionPositions.Count == 0)
+                return;
+
+            connectionCount = Math.Min(connectionCount, connectionPositions.Count - 1);
+            if (Input.GetKeyUp(KeyCode.R)) {
+                connectionCount = ++connectionCount % connectionPositions.Count;
+            }
+
+
+            t_gameStateGame_mActiveModule.Field<GameObject>("mObject").Value.transform.localRotation = Quaternion.LookRotation((connectionPositions[connectionCount] - activeModule.getPosition()).normalized);
+        }
+    }
+}
---
+++ AutoRotateBuildings/Main.cs
@@ -0,0 +1,65 @@
+﻿using Planetbase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Reflection;
+using UnityModManagerNet;
+using HarmonyLib;
+
+
+namespace AutoAlerts {
+
+    public static class Main {
+
+        public static bool active;
+        private static Harmony harmony;
+
+        /// <summary>
+        /// Send a response to the mod manager about the launch status, success or not.
+        /// </summary>
+        /// <param name="modEntry"></param>
+        /// <returns></returns>
+        public static bool Load(UnityModManager.ModEntry modEntry) {
+            modEntry.OnToggle = OnToggle;
+            harmony = new Harmony(modEntry.Info.Id);
+            return true; // If false the mod will show an error.
+        }
+
+        static bool doPatching(bool patch = true) {
+            bool result = true;
+            try {
+                if (patch) {
+                    harmony.PatchAll(Assembly.GetExecutingAssembly());
+                }
+                else {
+                    harmony.UnpatchAll();
+                }
+            }
+            catch (Exception) {
+                result = false;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Called when the mod is turned to on/off.
+        /// With this function you control an operation of the mod and inform users whether it is enabled or not.
+        /// </summary>
+        /// <param name="modEntry"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value /* active or inactive */) {
+            bool result = true;
+            if (active != value) {
+                result = doPatching(value);
+                if (result) {
+                    active = value;
+                }
+            }
+            return result; // If true, the mod will switch the state. If not, the state will not change.
+        }
+
+    }
+}
