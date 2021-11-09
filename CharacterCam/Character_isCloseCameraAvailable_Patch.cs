a512bddcac489b005b96a117e71366990807e3f5
Andronis Adamantios <adamantios.andronis@outlook.com.gr>
2021-11-09 01:31:26 +0000

    CharacterCam

---
+++ CharacterCam/Character_isCloseCameraAvailable_Patch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using Planetbase;
+using System.Collections.Generic;
+using System.Reflection;
+using System;
+using System.Reflection.Emit;
+
+namespace CharacterCam {
+
+    [HarmonyPatch(typeof(Character), "isCloseCameraAvailable", MethodType.Normal)]
+    static class Character_isCloseCameraAvailable_Patch {
+
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler(this IEnumerable<CodeInstruction> instructions) {
+            return new CodeInstruction[]{
+                new CodeInstruction(OpCodes.Ldc_I4_1),
+                new CodeInstruction(OpCodes.Ret)
+            };
+        }
+
+    }
+}
---
+++ CharacterCam/CloseCameraCinematic_updateCharacter_Patch.cs
@@ -0,0 +1,40 @@
+﻿using Planetbase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using HarmonyLib;
+
+namespace CharacterCam {
+
+    [HarmonyPatch(typeof(CloseCameraCinematic), "updateCharacter", MethodType.Normal)]
+    static class GameGuiManager_setTimeScale_Patch {
+
+        [HarmonyPrefix]
+        static bool Prefix(CloseCameraCinematic __instance, Character character, float timeStep) {
+            Traverse t___instance = Traverse.Create(__instance);
+            Traverse<float> t_mLastRotation = t___instance.Field<float>("mLastRotation");
+
+            Transform cameraTransform = CameraManager.getInstance().getTransform();
+            Transform characterTransform = character.getTransform();
+
+            float yAngle = characterTransform.eulerAngles.y;
+            float horizontalBobbing = Mathf.Clamp((t_mLastRotation.Value - yAngle) * 0.25f, -0.5f, 0.5f);
+            Vector3 newPos = characterTransform.position + Vector3.up * character.getHeight() + characterTransform.forward * 0.7f + horizontalBobbing * characterTransform.right;
+            if (t___instance.Field<bool>("mFirstUpdate").Value) {
+                cameraTransform.position = newPos;
+                cameraTransform.rotation = characterTransform.rotation;
+                t_mLastRotation.Value = yAngle;
+            }
+            cameraTransform.position = Vector3.Lerp(cameraTransform.position, newPos, 0.1f);
+            Vector3 lookAtDir = (characterTransform.forward * 1.4f + Vector3.up * (character.getHeight() * 0.85f)).normalized;
+            cameraTransform.rotation = Quaternion.RotateTowards(cameraTransform.rotation, Quaternion.LookRotation(lookAtDir), timeStep * 120f);
+            t_mLastRotation.Value = yAngle;
+
+            return false;
+        }
+        
+    }
+
+}
